6a5986c882292d3938a1a5bbce81a39d873b132b
Emmanuel Makafui Agbogan <agboganemmanuel@gmail.com>
2024-12-11 08:39:22 -0500

    dashborad made more responsive

--- src/app/dashboard/_components/Card.tsx
+++ src/app/dashboard/_components/Card.tsx
@@ -39,16 +39,16 @@ export function DashboardCards() {
   ]
 
   return (
-    <div className="flex justify-center space-x-4 w-full">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 w-full px-4 md:px-8 lg:px-12">
       {cardData.map((card, index) => (
-        <div key={index} className="card bg-base-100 w-64 shadow-xl">
-          <div className="card-body">
+        <div key={index} className="card bg-base-100 shadow-xl flex-grow">
+          <div className="card-body flex-1">
             <div className="flex justify-between items-center">
-              <h2 className="card-title text-xl text-base-content">{card.title}</h2>
-              <card.icon className={`h-8 w-8 ${card.iconColor}`} />
+              <h2 className="card-title text-base sm:text-lg md:text-xl text-base-content">{card.title}</h2>
+              <card.icon className={`h-6 w-6 sm:h-8 sm:w-8 ${card.iconColor}`} />
             </div>
-            <p className="text-3xl font-bold text-base-content">{card.value}</p>
-            <p className="text-sm text-base-content">{card.description}</p>
+            <p className="text-2xl sm:text-3xl font-bold text-base-content">{card.value}</p>
+            <p className="text-xs sm:text-sm text-base-content">{card.description}</p>
           </div>
         </div>
       ))}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -14,11 +14,11 @@ export default function Dashboard() {
       <main className="flex-grow pt-24 flex flex-col items-center space-y-6 px-4 sm:px-6 lg:px-60"> 
         <h1 className="text-3xl sm:text-4xl lg:text-5xl mt-6 font-bold text-base-content text-center">Dashboard</h1>
         <DashboardCards />
-        <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 w-full">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4 w-full px-4 md:px-8 lg:px-12">
           <RecentUsers />
           <RecentSales />
         </div>
-        <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 w-full">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4 w-full px-4 md:px-8 lg:px-12">
           <UserLineGraph />
           <SalesBarChart />
         </div>
